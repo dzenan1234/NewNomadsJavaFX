20cb1799fb5ffcab2f3837d6d88711b4b4294a87
Davud Skelic <davudskelic@Davuds-MacBook-Air.local>
2025-12-09 18:13:41 +0100

    Nakon sto sam kreirao bazu, modificirao sam db.java tako da sada radi sa stvarnom mysql bazom, te dodao dbtest.java fajl koji sluzi za testiranje konekcije

--- src/main/java/com/example/newnomads/DB.java
+++ src/main/java/com/example/newnomads/DB.java
@@ -5,13 +5,21 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DB {
-    private static final String URL = "jdbc:mysql://localhost:3306/newnomads?serverTimezone=UTC&useSSL=false";
-    private static final String USER = "root";
-    private static final String PASS = "root!";
 
-    public static Connection getConnection() throws SQLException, ClassNotFoundException {
-        // Registracija MySQL drivera
-        Class.forName("com.mysql.cj.jdbc.Driver");
+    private static final String URL =
+            "jdbc:mysql://newnomadsbaza-newnomadsapp.d.aivencloud.com:10105/newnomadsbaza" +
+                    "?sslMode=REQUIRED" +
+                    "&useUnicode=true" +
+                    "&characterEncoding=utf8" +
+                    "&serverTimezone=UTC";
+
+    private static final String USER = "newnomadskorisnik";
+    private static final String PASS = System.getenv("DB_PASSWORD");
+
+    public static Connection getConnection() throws SQLException {
+        if (PASS == null || PASS.isBlank()) {
+            throw new SQLException("DB_PASSWORD environment variable nije postavljena!");
+        }
         return DriverManager.getConnection(URL, USER, PASS);
     }
 }
