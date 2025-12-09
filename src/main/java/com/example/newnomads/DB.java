2cb96ee183a4396421356b63eeec512084d1ddcb
Davud Skelic <davudskelic@Davuds-MacBook-Air.local>
2025-12-09 19:07:57 +0100

    Ispravka za konekciju sa bazom radi lakse konekcije da s ene mora svaki put unosit env, izmjenjeni sugitignor  pom.xml i DB

--- src/main/java/com/example/newnomads/DB.java
+++ src/main/java/com/example/newnomads/DB.java
@@ -1,10 +1,13 @@
 package com.example.newnomads;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DB {
+
+    private static final Dotenv dotenv = Dotenv.load();
 
     private static final String URL =
             "jdbc:mysql://newnomadsbaza-newnomadsapp.d.aivencloud.com:10105/newnomadsbaza" +
@@ -14,11 +17,11 @@ public class DB {
                     "&serverTimezone=UTC";
 
     private static final String USER = "newnomadskorisnik";
-    private static final String PASS = System.getenv("DB_PASSWORD");
+    private static final String PASS = dotenv.get("DB_PASSWORD");
 
     public static Connection getConnection() throws SQLException {
         if (PASS == null || PASS.isBlank()) {
-            throw new SQLException("DB_PASSWORD environment variable nije postavljena!");
+            throw new SQLException("DB_PASSWORD nije postavljena u .env fajlu!");
         }
         return DriverManager.getConnection(URL, USER, PASS);
     }
